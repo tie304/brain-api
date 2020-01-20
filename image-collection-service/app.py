a23cab00bdcd96d371b984211fca6f58ff21254b
Tyler <tylerhanson921@gmail.com>
2020-01-19 18:38:30 -0600

    changed redis port

--- image-collection-service/app.py
+++ image-collection-service/app.py
@@ -1,13 +1,14 @@
+import os
 import json
 from redis_conn import RedisConn
 from src.image_collector import  GoogleImageCollector
 
-RedisConn.initialize('redis', 6379)
+RedisConn.initialize(os.environ.get("REDIS_HOST"), os.environ.get("REDIS_PORT"))
 
 data = RedisConn.CONN.blpop('gathering-queue') # blocking operation until data enters queue
 gathering_data = json.loads(data[1]) # load json string from redis
 
-print(gathering_data.get('max_images'))
+
 
 collector = GoogleImageCollector(project=gathering_data.get('project'),
                                  subclass = gathering_data.get('subclass'),
